277f77a3b95823cc2b39558a9d6d3758ebb39900
kumaw <atmwjf@163.com>
2015-12-30 15:47:58 +0800

    解析组件依赖

--- astro-js-dep.js
+++ astro-js-dep.js
@@ -21,113 +21,119 @@ module.exports = new astro.Middleware({
         unCombine: []
     }, asset.prjCfg);
     getJsDependent(asset, function(errorMsg, jsLibs) {
-
         asset.jsLibs = [errorMsg, jsLibs];
-        next(asset);return;
-        let jsLibCode = '',
-            unCombined = [],
-            combined = [],
-            tmpJslibs = jsLibs;
-        // 加载所有JS组件
-        jsLibs = jsLibs.map(function(js) {
-            if (util.inArray(js, prjCfg.unCombined)) {
-                unCombined.push(prjCfg.source[js] || js);
-            } else {
-                combined.push(js);
-                return new astro.Asset({
-                    ancestor: asset,
-                    modType: 'jsCom',
-                    fileType: 'js',
-                    name: js,
-                    project: project
-                });
-            }
-        });
-
-        let reader = astro.Asset.getContents(jsLibs);
-        
-        reader.then(function(assets) {
-            var errorMsg = '';
-            assets.forEach(function(at) {
-                if (at.data) {
-                    jsLibCode += ['','/* '+ at.filePath +' */', at.data, ''].join('\n');
-                    return;
-                }
-                errorMsg += nodeUtil.format('\n/* jsLib(%s) is miss, project:%s */', js, project);
-            });
-            try{
-                jsLibCode = '/* unCombined:' + unCombined.join(',') + ' */\n/* jsCom:' + combined.join(',') + ' */ \n' + jsLibCode + '\n';
-                asset.data = [errorMsg, jsLibCode, '/* ' + asset.filePath+ ' */', asset.data].join('\n');
-                asset.jsLibs = tmpJslibs;
-                next(asset);
-            }catch(e){
-                console.log(e);
-            }
-        })
+        next(asset);
     });
 });
 
 let refer_cache = {};
 // 获取代码里的引用关系
-function getReference(asset) {
-    let cache = refer_cache[asset.filePath] || {};
-    if(cache.mtime !== asset.mtime){
-        let ret = [];
-        (asset.data||'').replace(/@require\s+(\S+)/g, function(a, reqjs) {
-            reqjs.split(',').forEach(function(item){
-                item = item.replace(/^\s|\s$/,'');
-                if(item){
-                    ret.push(item)
-                }
+function getReference(asset,callback) {
+    //读取组件代码
+    var tempDate = '';
+    let webComCode = '',
+        components = [];
+    if (asset.components && asset.components.length) {
+        components = asset.components.map(function(wc) {
+            return new astro.Asset({
+                ancestor: asset,
+                project: asset.project,
+                modType: 'webCom',
+                name: wc,
+                fileType: 'js'
             });
-        });
-        cache.data = ret;
-        cache.mtime = asset.mtime;
-
-        refer_cache[asset.filePath] = cache;
+        })
     }
-    return cache.data;
+    //console.log(components);
+    let reader = astro.Asset.getContents(components);
+
+    reader.then(function(assets) {
+        var wcError = '';
+        assets.forEach(function(ast) {
+            if (!ast.data)
+                wcError += ['/* ', 'webCom:' + ast.filePath + ' is miss */', ''].join('\n');
+            else {
+                webComCode += '/* ' + ast.filePath + ' */\n' + ast.data + '\n';
+            }
+        });
+        tempDate = webComCode + (tempDate || '');
+        // 读取依赖组件
+        tempDate = tempDate + asset.data;
+        let cache = refer_cache[asset.filePath] || {};
+        if(cache.mtime !== asset.mtime){
+            let ret = [];
+            (tempDate||'').replace(/@require\s+(\S+)/g, function(a, reqjs) {
+                reqjs.split(',').forEach(function(item){
+                    item = item.replace(/^\s|\s$/,'');
+                    if(item){
+                        ret.push(item)
+                    }
+                });
+            });
+            cache.data = ret;
+            cache.mtime = asset.mtime;
+
+            refer_cache[asset.filePath] = cache;
+        }
+        callback(cache.data);
+        
+    }).catch(function(error) {
+        console.error('astro-js-process', error);
+        asset.data = error + '\n' + asset.data
+        next(asset);
+    });
+
 }
 
 function getJsDependent(asset, callback) {
     let errorMsg = '';
-    let jsLibs = getReference(asset);
-    //处理依赖
-    if (jsLibs.length > 0) {
-        // 处理JS组件依赖关系
-        let process = (function*() {
-            let i = 0;
-            while (jsLibs[i]) {
-                if (i > 1000) {
-                    errorMsg += '/* ***** ' + '\n依赖套嵌超过一千次，可能出现死循环\n' + jsLibs.join(',') + '** */\n';
-                    console.error('n依赖套嵌超过一千次，可能出现死循环, asset.name:%s, asset.components', asset.name, asset.components ? asset.components.join(',') : 'null');
-                    console.info(jsLibs.join(','));
-                    break;
-                }
-                new astro.Asset({
-                    ancestor: asset,
-                    modType: 'jsCom',
-                    fileType: 'js',
-                    name: jsLibs[i],
-                    project: asset.project
-                }).getContent(function(asset) {
-                    if (!asset.data) {
-                        errorMsg += '/* jsLib:' + jsLibs[i] + ' is miss or empty */\n';
-                    } else {
-                        jsLibs = jsLibs.concat(getReference(asset));
+    getReference(asset,function(jsLibs){
+         //处理依赖
+        if (jsLibs.length > 0) {
+            // 处理JS组件依赖关系
+            let process = (function*() {
+                let i = 0;
+                while (jsLibs[i]) {
+                    if (i > 1000) {
+                        errorMsg += '/* ***** ' + '\n依赖套嵌超过一千次，可能出现死循环\n' + jsLibs.join(',') + '** */\n';
+                        console.error('n依赖套嵌超过一千次，可能出现死循环, asset.name:%s, asset.components', asset.name, asset.components ? asset.components.join(',') : 'null');
+                        console.info(jsLibs.join(','));
+                        break;
                     }
-                    i++;
-                    process.next();
-                });
-                yield;
-            }
+                    new astro.Asset({
+                        ancestor: asset,
+                        modType: 'jsCom',
+                        fileType: 'js',
+                        name: jsLibs[i],
+                        project: asset.project
+                    }).getContent(function(asset) {
+                        if (!asset.data) {
+                            errorMsg += '/* jsLib:' + jsLibs[i] + ' is miss or empty */\n';
+                            i++;
+                            process.next();
+                        } else {
+                            getReference(asset,function(tempJsLibs){
+                                jsLibs = jsLibs.concat(tempJsLibs);
+                                i++;
+                                process.next();
+                            });
+                        }
+                        // i++;
+                        // process.next();
+                    });
+                    yield;
+                }
+                done();
+            }());
+            process.next();
+        } else {
             done();
-        }());
-        process.next();
-    } else {
-        done();
-    }
-    function done() {
-        callback(errorMsg, util.dequeueArray(jsLibs).reverse());
-    }
+        }
+        function done() {
+                        console.log(jsLibs);
+            callback(errorMsg, util.dequeueArray(jsLibs).reverse());
+        }
+
+    });
+   
 }
